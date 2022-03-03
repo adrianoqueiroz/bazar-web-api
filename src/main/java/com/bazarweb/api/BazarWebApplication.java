74bf4bf15f580c793cae6a2d0a06edf7650e8b57
Adriano Novais Queiroz <adriano.nq@gmail.com>
2022-03-02 21:55:06 -0300

    Project template

---
+++ src/main/java/com/bazarweb/api/BazarWebApplication.java
@@ -0,0 +1,13 @@
+package com.bazarweb.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bazarweb/api/BazarWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bazarweb.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazarWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
